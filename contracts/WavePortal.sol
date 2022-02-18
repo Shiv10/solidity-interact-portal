77cb8ae60b78c4f26d4074f5fd4f7f3a5a1b2b3f
Shiv10 <shivanshsharma2012@gmail.com>
2022-02-18 21:46:16 +0530

    feat: add functionality

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,18 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalJokes;
     constructor() {
         console.log("Hey this is my first smart contract!!");
+    }
+
+    function joke() public {
+        totalJokes += 1; 
+        console.log("%s has cracked a joke", msg.sender);
+    }
+
+    function getTotalJokes() public view returns(uint256) {
+        console.log("We have %d total jokes", totalJokes);
+        return totalJokes;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,23 @@
 const main = async () => {
+
+    const [owner, randomPerson] = await hre.ethers.getSigners()
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to: ", waveContract.address);
+    console.log("Contract deployed by: ", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalJokes();
+
+    let waveTxn = await waveContract.joke();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalJokes();
+
+    waveTxn = await waveContract.connect(randomPerson).joke();
+    await waveTxn.wait();
+    waveCount = await waveContract.getTotalJokes();
 }
 
 const runMain = async () => {
